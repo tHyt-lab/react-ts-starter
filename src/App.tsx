199774d69a518082cfcc939b070c704d1152215f
tHyt-lab <washers.fezzes-0q@icloud.com>
2024-08-22 03:46:28 +0000

    test: linter violation

--- src/App.tsx
+++ src/App.tsx
@@ -18,10 +18,7 @@ function App() {
 			</div>
 			<h1>Vite + React</h1>
 			<div className="card">
-				<button
-					type="button"
-					onClick={() => setCount((prevCount) => prevCount + 1)}
-				>
+				<button onClick={() => setCount((prevCount) => prevCount + 1)}>
 					count is {count}
 				</button>
 				<p>
