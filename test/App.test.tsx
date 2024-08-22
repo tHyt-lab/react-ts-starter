e73b3fcb7a6be65fe9efd53f3e6288b720c1dc33
tHyt-lab <washers.fezzes-0q@icloud.com>
2024-08-22 03:19:10 +0000

    refactor: Update App component to use consistent indentation

--- test/App.test.tsx
+++ test/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 import App from "../src/App";
 
 test("Vite+ReactのWelcomeページが表示されている", () => {
-  render(<App />);
-  screen.debug();
-  expect(screen.getByText("Vite + React")).toBeInTheDocument();
+	render(<App />);
+	screen.debug();
+	expect(screen.getByText("Vite + React")).toBeInTheDocument();
 });
